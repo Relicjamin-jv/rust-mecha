d0c81d59b0ab3f0a849032ac6f25117851250066
Collin Campbell <collincampbell97@proton.me>
2023-11-01 22:20:26 -0400

    Add Timer and Default Plugins
    The game loop has been made! The DefaultPlugins loads in the whole
    game loop, include the RenderPlugin. We now have a screen.

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,47 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Component)]
+struct Position {x : f32, y: f32}
+
+fn print_position_system(query: Query<&Position>){
+    for position in &query{
+        println!("position: {} {}", position.x, position.y)
+    }
+}
+
+pub struct HelloPlugin;
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App){
+        // Add some plugin stuff
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
+}
+
+fn hello_world(){
+    println!("Hello World")
+}
+
 fn add_people(mut commands: Commands){
     commands.spawn((Person, Name("Collin".to_string())));
     commands.spawn((Person, Name("Tanya".to_string())));
 }
 
-fn hello_world(){
-    println!("Hello World!");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
-fn greet_people(query: Query<&Name, With<Person>>){
-    for name in &query {
-        println!("Hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer> ,query: Query<&Name, With<Person>>){
+    if timer.0.tick(time.delta()).just_finished(){
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people)).run();
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
 }
