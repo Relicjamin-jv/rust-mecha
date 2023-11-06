f6b4774edbbea95205452da391c6ebb35cb76533
Collin Campbell <collincampbell97@proton.me>
2023-11-05 19:14:22 -0500

    Add Relative Import for Loading in Sprites
    Some code that loads in Bevy Sprites!

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,16 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Component)]
-struct Position {x : f32, y: f32}
-
-fn print_position_system(query: Query<&Position>){
-    for position in &query{
-        println!("position: {} {}", position.x, position.y)
-    }
-}
-
-pub struct HelloPlugin;
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App){
-        // Add some plugin stuff
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, greet_people);
-    }
-}
-
-fn hello_world(){
-    println!("Hello World")
-}
-
-fn add_people(mut commands: Commands){
-    commands.spawn((Person, Name("Collin".to_string())));
-    commands.spawn((Person, Name("Tanya".to_string())));
-}
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer> ,query: Query<&Name, With<Person>>){
-    if timer.0.tick(time.delta()).just_finished(){
-        for name in &query {
-            println!("Hello {}!", name.0);
-        }
-    }
-}
-
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("resources/TopDownMechaArt/mockuprf23.PNG"),
+        ..default()
+    });
 }
